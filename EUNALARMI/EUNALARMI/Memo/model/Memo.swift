9bf59dacc70e7446acb6ae01dad3ffc08ec527bb
EunYeongKim <povoq1024@gmail.com>
2020-09-24 15:01:49 +0900

    [ALA-4] 메모 앱 메인 뷰 작성 * CollectionView * TableView --- * CollectionViewCell Extension생성 border and shadow 적용

---
+++ EUNALARMI/EUNALARMI/Memo/model/Memo.swift
@@ -0,0 +1,26 @@
+//
+//  Memo.swift
+//  EUNALARMI
+//
+//  Created by 60080252 on 2020/09/23.
+//  Copyright © 2020 60080252. All rights reserved.
+//
+
+import Foundation
+
+class Memo {
+    var content: String
+    var createDate: Date
+    
+    init(content: String) {
+        self.content = content
+        createDate = Date()
+    }
+    
+    static var dummyMemoList = [
+    Memo(content: "Lorem Ipsum"),
+    Memo(content: "Subscribe + 👍 = ❤️"),
+    Memo(content: "나는 은영이양 하이루"),
+    Memo(content: "호호호호ㅗ호 퇵은 퇵은 하고 싶어요")
+    ]
+}
